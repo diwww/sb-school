167218d95ebe9db953fc64d1c48089a70c5368e1
Maxim Surovtsev <realitybelieves@gmail.com>
2018-08-03 11:58:03 +0300

    Seminar23: update.

--- practice/Seminar23-CustomViewGroup/app/src/main/java/ru/gcsales/seminar23/MainActivity.java
+++ practice/Seminar23-CustomViewGroup/app/src/main/java/ru/gcsales/seminar23/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int CHILD_COUNT = 13;
+    public static final int CHILD_COUNT = 50;
     private MyViewGroup mMyViewGroup;
 
     @Override
@@ -20,8 +20,9 @@ public class MainActivity extends AppCompatActivity {
         LayoutInflater layoutInflater = getLayoutInflater();
         for (int i = 0; i < CHILD_COUNT; i++) {
             View view = layoutInflater.inflate(R.layout.view, null, false);
-            TextView textView = view.findViewById(R.id.text_view);
-            textView.setText("Text #" + i);
+            if (view instanceof TextView) {
+                ((TextView) view).setText("Text #" + i);
+            }
             mMyViewGroup.addView(view);
         }
     }
--- practice/Seminar23-CustomViewGroup/app/src/main/java/ru/gcsales/seminar23/MyViewGroup.java
+++ practice/Seminar23-CustomViewGroup/app/src/main/java/ru/gcsales/seminar23/MyViewGroup.java
@@ -1,12 +1,10 @@
 package ru.gcsales.seminar23;
 
 import android.content.Context;
-import android.graphics.Point;
 import android.util.AttributeSet;
-import android.view.Display;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 
 /**
  * @author Maxim Surovtsev
@@ -14,87 +12,63 @@ import android.view.WindowManager;
  */
 public class MyViewGroup extends ViewGroup {
 
-    private int screenHeight = 0;
-    private int screenWidth = 0;
+    private static final String TAG = "MyViewGroup";
 
-
-    public MyViewGroup(Context context) {
-        this(context, null, 0);
-    }
 
     public MyViewGroup(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    public MyViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        Display display = ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        screenHeight = size.y;
-        screenWidth = size.x;
+        super(context, attrs);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int count = getChildCount();
-        int maxHeight = 0;
-        int maxWidth = 0;
-        int childState = 0;
-        int height = 0;
-        int rowCount = 0;
+        String log1 = String.format("w: %s, h: %s", MeasureSpec.toString(widthMeasureSpec), MeasureSpec.toString(heightMeasureSpec));
+        Log.d(TAG, log1);
 
-        for (int i = 0; i < count; i++) {
-            final View child = getChildAt(i);
+        int desiredWidth = 0;
+        // Always match_parent
+        final int height = MeasureSpec.getSize(heightMeasureSpec);
 
-            if (child.getVisibility() != GONE) {
-                measureChild(child, widthMeasureSpec, heightMeasureSpec);
-                maxHeight = maxHeight + Math.max(maxHeight, child.getMeasuredHeight());
-                height = height + child.getMeasuredHeight();
-                if ((height / screenHeight) > rowCount) {
-                    maxWidth = maxWidth + child.getMeasuredWidth();
-                    rowCount++;
-                } else {
-                    maxWidth = Math.max(maxWidth, child.getMeasuredWidth());
-                }
-                childState = combineMeasuredStates(childState, child.getMeasuredState());
+        int childCount = getChildCount();
+        // Height of a single column. This should be
+        // always less than height of this ViewGroup.
+        // Initial value is equal to height to generate
+        // the first column.
+        int columnHeight = height;
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            // If a column can fit one more child
+            if (columnHeight + child.getMeasuredHeight() < height) {
+                columnHeight += child.getMeasuredHeight();
+            } else { // Else add one more column
+                columnHeight = 0;
+                desiredWidth += child.getMeasuredWidth();
             }
         }
 
-        maxHeight = Math.max(maxHeight, getSuggestedMinimumHeight());
-
-        setMeasuredDimension(resolveSizeAndState(maxWidth, widthMeasureSpec, childState),
-                resolveSizeAndState(maxHeight, heightMeasureSpec, childState << MEASURED_HEIGHT_STATE_SHIFT));
-
+        setMeasuredDimension(resolveSize(desiredWidth, widthMeasureSpec), heightMeasureSpec);
     }
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        final int count = getChildCount();
-        int curWidth, curHeight, curLeft, curTop, maxHeight;
+        int childCount = getChildCount();
 
-        final int childLeft = getPaddingLeft();
-        final int childTop = getPaddingTop();
-        final int childRight = getMeasuredWidth() - getPaddingRight();
-        final int childBottom = getMeasuredHeight() - getPaddingBottom();
-        final int childWidth = childRight - childLeft;
-        final int childHeight = childBottom - childTop;
+        final int height = getMeasuredHeight();
 
-        curLeft = childLeft;
-        curTop = childTop;
-        for (int i = 0; i < count; i++) {
+        // Starting points
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < childCount; i++) {
             View child = getChildAt(i);
-            if (child.getVisibility() != GONE) {
-                child.measure(MeasureSpec.makeMeasureSpec(childWidth, MeasureSpec.AT_MOST), MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.AT_MOST));
-                curWidth = child.getMeasuredWidth();
-                curHeight = child.getMeasuredHeight();
-                if (curTop + curHeight >= childBottom) {
-                    curTop = 0;
-                    curLeft += curWidth;
-                }
 
-                child.layout(curLeft, curTop, curLeft + curWidth, curTop + curHeight);
-                curTop += curHeight;
+            if (y + child.getMeasuredHeight() < height) {
+                child.layout(x, y, x + child.getMeasuredWidth(), y + child.getMeasuredHeight());
+                y += child.getMeasuredHeight();
+            } else {
+                x += child.getMeasuredWidth();
+                y = 0;
+                child.layout(x, y, x + child.getMeasuredWidth(), y + child.getMeasuredHeight());
             }
         }
     }
